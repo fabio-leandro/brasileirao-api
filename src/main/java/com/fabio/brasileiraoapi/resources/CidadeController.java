72bd20cb5138941b314a6679a005ea93f6dd2cbc
root <root@DESKTOP-E4TOMJ2.localdomain>
2022-04-12 14:52:25 -0300

    Cidade Controller, metodos save e getall

---
+++ src/main/java/com/fabio/brasileiraoapi/resources/CidadeController.java
@@ -0,0 +1,38 @@
+package com.fabio.brasileiraoapi.resources;
+
+import com.fabio.brasileiraoapi.domains.Cidade;
+import com.fabio.brasileiraoapi.repositories.CidadeRepository;
+import com.fabio.brasileiraoapi.repositories.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/${api.version}/cidades")
+public class CidadeController {
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @PostMapping
+    public Mono<ResponseEntity<Cidade>> saveCidade(@RequestBody Cidade cidade){
+        return estadoRepository.findBySigla(cidade.getEstado().getSigla())
+                .flatMap(a ->{
+                    cidade.setEstado(a);
+                    return cidadeRepository.save(cidade);
+                }).map(c -> ResponseEntity.status(HttpStatus.CREATED).body(c))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<Flux<Cidade>> getAll(){
+        return ResponseEntity.ok(cidadeRepository.findAll());
+    }
+
+}
